ba94a6278b6d842acf409d01bd2f95fbac9ea2eb
Haha <jis252@eng.ucsd.edu>
2018-02-21 02:35:23 -0800

    tt test

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -43,10 +43,10 @@ authRouter.route('/facebook/callback')
 )
 
 authRouter.route('/twitter')
-.get(passport.authenticate('facebook'),(req, res)=>console.log(res))
+.get(passport.authenticate('twitter'),(req, res)=>console.log(res))
 
 authRouter.route('/twitter/callback')
-.get(passport.authenticate('facebook', { failureRedirect: '/auth' }),
+.get(passport.authenticate('twitter', { failureRedirect: '/auth' }),
 	(req, res, next) => {
     	// Successful authentication, redirect home.
     	res.status(200).redirect('/');
