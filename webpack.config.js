e3cc56f704613b74d51b0296884959847091ccd0
Sun <sunjingzhe93@gmail.com>
2019-06-25 22:52:33 +0800

    minor

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
             { test: /\.json$/, loader: 'json-loader' },
             { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
             { test: /\.(woff|woff2)$/, loader:"url-loader?prefix=font/&limit=5000" },
-        　　  { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
             { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" },
             { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
         ]
